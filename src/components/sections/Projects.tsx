be73955d0ffb39872654dd0f71807c50c2ae6b28
nawalkhanz08-commits <nawalkhanz08@gmail.com>
2026-07-31 18:53:02 +0500

    remove the button of github and live demo

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { Github } from '../ui/Icons';
-import { ExternalLink } from 'lucide-react';
+
 
 const projects = [
   {
